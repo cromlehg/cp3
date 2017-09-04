91ec9fdc777187e297821c6b4254d246cf107b83
Alexander Strakh <strakh@ispras.ru>
2017-09-04 14:38:45 +0300

    Improve tests

--- test/commonSale.js
+++ test/commonSale.js
@@ -34,7 +34,8 @@ contract('CommonSale', function(wallets) {
 
   beforeEach(async function () {
     this.token = await Token.new()
-    this.crowdsale = await CommonSale.new(Token(this.token))
+    this.crowdsale = await CommonSale.new()
+    await this.crowdsale.setToken(this.token.address)
   })	 
 
   it('should be token owner', async function () {
@@ -53,4 +54,59 @@ contract('CommonSale', function(wallets) {
     await this.token.transferOwnership(newOwner, {from: notOwner}).should.be.rejectedWith(EVMThrow)
   })	 
 
+  it('finish minting should reject if not owner', async function () {
+    await this.token.finishMinting({from: notOwner}).should.be.rejectedWith(EVMThrow)
+  })	 
+
+  it('owner should changed', async function () {
+    await this.token.transferOwnership(newOwner, {from: owner})
+    newOwner.should.equal(await this.token.owner())
+    await this.token.transferOwnership(owner, {from: newOwner})
+    owner.should.equal(await this.token.owner())
+  })	 
+
+
+  const m0Period = new BigNumber(1)
+  const m1Period = new BigNumber(2)
+  const m2Period = new BigNumber(3)
+  const m3Period = new BigNumber(4)
+  const m0Bonus = new BigNumber(10)
+  const m1Bonus = new BigNumber(20)
+  const m2Bonus = new BigNumber(30)
+  const m3Bonus = new BigNumber(40)
+
+  it('common sale should add/insert/remove/clear/change milestone', async function () {
+    await this.crowdsale.addMilestone(m0Period, m0Bonus, {from: owner})
+    const m0 = await this.crowdsale.milestones(0)
+    m0[0].should.be.bignumber.equal(m0Period)
+    m0[1].should.be.bignumber.equal(m0Bonus)
+    const count0 = await this.crowdsale.milestonesCount()
+    count0.should.be.bignumber.equal(1)
+    m0Period.should.be.bignumber.equal(await this.crowdsale.totalPeriod())
+    await this.crowdsale.addMilestone(m1Period, m1Bonus, {from: owner})
+    const m1 = await this.crowdsale.milestones(1)
+    m1[0].should.be.bignumber.equal(m1Period)
+    m1[1].should.be.bignumber.equal(m1Bonus)
+    const count1 = await this.crowdsale.milestonesCount()
+    count1.should.be.bignumber.equal(2)
+    m0Period.add(m1Period).should.be.bignumber.equal(await this.crowdsale.totalPeriod())
+    await this.crowdsale.addMilestone(m3Period, m3Bonus, {from: owner})
+    const m3 = await this.crowdsale.milestones(2)
+    m3[0].should.be.bignumber.equal(m3Period)
+    m3[1].should.be.bignumber.equal(m3Bonus)
+    const count2 = await this.crowdsale.milestonesCount()
+    count2.should.be.bignumber.equal(3)
+    m0Period.add(m1Period).add(m3Period).should.be.bignumber.equal(await this.crowdsale.totalPeriod())
+    await this.crowdsale.insertMilestone(m1Period, m2Period, m2Bonus, {from: owner})
+    const m2 = await this.crowdsale.milestones(3)
+    m2[0].should.be.bignumber.equal(m2Period)
+    m2[1].should.be.bignumber.equal(m2Bonus)
+    const count3 = await this.crowdsale.milestonesCount()
+    count3.should.be.bignumber.equal(4)
+    m0Period.add(m1Period).add(m2Period).add(m3Period).should.be.bignumber.equal(await this.crowdsale.totalPeriod())
+    // TODO: change it
+    // TODO: remove it
+    // clear all
+  })	 
+ 
 })
